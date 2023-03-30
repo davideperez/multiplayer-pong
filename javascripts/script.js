c72fd4a02d2fa4ddd555ce629b39b40e6c578ba3
David Perez <david.ezequiel.pere@gmail.com>
2023-03-30 16:14:49 -0300

    The ball x and y position, and the score is now in sync.

--- javascripts/script.js
+++ javascripts/script.js
@@ -90,16 +90,29 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
+
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score
+  })
 }
 
 // Adjust Ball Movement
 function ballMove() {
   // Vertical Speed
   ballY += speedY * ballDirection;
+  
   // Horizontal Speed
   if (playerMoved) {
     ballX += speedX;
   }
+
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score
+  })
 }
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
@@ -155,9 +168,11 @@ function ballBoundaries() {
 
 // Called Every Frame
 function animate() {
-   ballMove();
+  if (isReferee) {
+    ballMove();
+    ballBoundaries();
+  }
   renderCanvas();
-  ballBoundaries();
   window.requestAnimationFrame(animate);
 }
 
@@ -207,6 +222,10 @@ socket.on('startGame', (refereeId) => {
 socket.on('paddleMove', (paddleData) => {
   //Toggle 1 into 0 and a 0 into a 1.
   const opponentPaddleIndex = 1 - paddleIndex;
-  
+
   paddleX[opponentPaddleIndex] = paddleData.xPosition;
+})
+
+socket.on('ballMove', (ballData) => {
+  ({ ballX, ballY, score } = ballData)
 })
