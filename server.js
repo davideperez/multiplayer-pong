f592839fdea520b6ecb0dbced2a3085c87d466f2
David Perez <david.ezequiel.pere@gmail.com>
2023-03-30 14:43:02 -0300

    io.emit function implemented to broadcast the game start, and aslo sends the last ready players sockets id. This will be used to set this player as the referee.

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
         readyPlayerCount++;
         
         if (readyPlayerCount === 2) {
-            //broadcast('startGame()')
+            io.emit('startGame', socket.id)//broadcast('startGame()')
         }
     })
 })
