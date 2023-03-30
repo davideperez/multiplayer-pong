7b012dd84bb63f4289addb18ebc5f482d177e825
David Perez <david.ezequiel.pere@gmail.com>
2023-03-30 14:31:03 -0300

    Plain socket.io server.js created. Socket.io Client implemented in html and in script.js. Computer logic erased. renderIntro implemented. Socket.id in both server and client logged by console and using socket.id functionality. socket.emit(ready) implemented.

---
+++ server.js
@@ -0,0 +1,22 @@
+//Plain socket.io server creation
+
+const server = require('http').createServer()
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
+
+const PORT = 3000
+
+server.listen(PORT);
+
+console.log(`Listening on port ${PORT}... `)
+
+
+//Event Listeners
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id)
+})
