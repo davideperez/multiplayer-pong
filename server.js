3405d942f9373a0fbe618d924ed54eda51db38d5
David <david.ezequiel.perez@gmail.com>
2023-04-03 17:10:37 -0300

    Express integrated with http and socket.io

--- server.js
+++ server.js
@@ -1,48 +1,20 @@
 //Plain socket.io server creation
+const http = require('http')
+const io = require('socket.io')
 
-const server = require('http').createServer()
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
-})
+       
+const apiServer = require('./api')
+const httpServer = http.createServer(apiServer)
+const sockets = require('./sockets')
+
+const socketServer = io(httpServer)
 
 const PORT = 3000
 
-server.listen(PORT);
+//Server Start
+
+httpServer.listen(PORT);
 
 console.log(`Listening on port ${PORT}... `)
 
-// VARs
-
-let readyPlayerCount = 0;
-
-
-//Event Listeners
-
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id)
-
-    socket.on('ready', () => {
-        console.log('Player ready', socket.id)
-        
-        readyPlayerCount++;
-        
-        if (readyPlayerCount % 2 === 0) {
-            io.emit('startGame', socket.id) //broadcast('startGame()') This way the second player will be the referee.
-        }
-    })
-
-    socket.on('paddleMove', (paddleData) => {
-        socket.broadcast.emit('paddleMove', paddleData)
-    })
-
-    socket.on('ballMove', (ballData) => {
-        socket.broadcast.emit('ballMove', ballData)
-    })
-
-    socket.on('disconnect', (reason) => {
-        console.log(`Client ${socket.id} disconected: ${reason}`)
-    } )
-})
+sockets.listen(socketServer)
